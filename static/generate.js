7b2a8a6813314b9de1c98c8bc2145d8f3cb02890
niemaattarian <G00346901@gmit.ie>
2019-11-14 22:49:43 +0100

    Updated web-app and flask

---
+++ static/generate.js
@@ -0,0 +1,75 @@
+/* Adapted from:
+https://www.codicode.com/art/how_to_draw_on_a_html5_canvas_with_a_mouse.aspx
+https://www.html5canvastutorials.com/labs/html5-canvas-paint-application/
+*/
+
+
+//<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+
+var mousePressed = false;
+var lastX, lastY;
+var ctx;
+var canvas;
+
+function InitThis() {
+    canvas = document.getElementById('myCanvas')
+    ctx = canvas.getContext("2d");
+
+
+
+    $('#myCanvas').mousedown(function (e) {
+        mousePressed = true;
+        Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+    });
+
+    $('#myCanvas').mousemove(function (e) {
+        if (mousePressed) {
+            Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
+        }
+    });
+
+    $('#myCanvas').mouseup(function (e) {
+        mousePressed = false;
+    });
+
+    $('#myCanvas').mouseleave(function (e) {
+        mousePressed = false;
+    });
+
+     $(document).ready(function (e) {
+        $("#predict").click(function (e) {
+
+            console.log(canvas.toDataURL());
+
+            $.post("/uploadImage", {"numberImage": canvas.toDataURL()}, function(data){
+            })
+        });
+    })
+    console.log(canvas.toDataURL());
+
+}
+
+function Draw(x, y, isDown) {
+    if (isDown) {
+        ctx.beginPath();
+        ctx.strokeStyle = $('#selColor').val();
+        ctx.lineWidth = $('#selWidth').val();
+        ctx.lineJoin = "round";
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(x, y);
+        ctx.closePath();
+        ctx.stroke();
+    }
+    lastX = x; lastY = y;
+}
+
+function clearArea() {
+    // Use the identity matrix while clearing the canvas
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+
+
+function predict(){
+
+}
--- web.py
+++ web.py
@@ -1,12 +1,30 @@
-from flask import Flask, escape, request, send_from_directory
+from idlelib.idle_test.test_browser import f1
 
+from flask import Flask, send_from_directory, render_template
+
+import base64
 app = Flask(__name__)
 
-
+# Add a route for the web-page
 @app.route('/')
 def web():
-    send_from_directory('web-app.html')
+    return render_template('web-app.html')
+
+
+# Add a route for the web-page
+@app.route('/uploadImage', methods=['GET', 'POST'])
+def uploadImage():
+    numberImage = f1.request.values.get("numberImage", "")
+    # Print to the console
+    print(numberImage)
+    # Decode the string to an image
+    decodedImage = base64.b64decode(numberImage[22:])
+    # Saving the image
+    with open("numberImage.png", "num") as f:
+        f.write(decodedImage)
 
 
 if __name__ == "__main__":
     app.run()
+
+
